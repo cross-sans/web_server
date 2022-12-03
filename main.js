7a8e77eca14c8ecd3636e2f9f66b411bdd436b34
cross <76446001+cross-sans@users.noreply.github.com>
2022-12-03 11:41:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+form = document.getElementById("form")
+form.addEventListener('submit', function(e) {
+	e.preventDefault()
+
+	const input = document.getElementById('file').files[0]
+	const comment = document.getElementById('comments').value
+
+	const data = new FormData()
+	data.append('inputed-file', input)
+	data.append('comment', comment)
+
+	fetch('https://httpbin.org/post', {
+		method: "POST",
+		body: data,
+	})
+	.then(res => res.json())
+	.then(data2 => console.log(data2))
+	.catch(err => console.log(err))
+
+
+
+
+})
